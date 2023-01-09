b023e73ee476d2334bf06f55e1f62c7c73c5f8f7
ajbt200128 <austin@cutedogs.org>
2023-01-09 18:54:05 -0500

    Packaging metadata

--- mix.exs
+++ mix.exs
@@ -1,6 +1,7 @@
 defmodule ExNar.MixProject do
   use Mix.Project
 
+  @source_url "https://github.com/ajbt200128/ex_nar"
   def project do
     [
       app: :ex_nar,
@@ -8,19 +9,40 @@ defmodule ExNar.MixProject do
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
       deps: deps()
+
+      # Hex
+      description: "A simple Nix Archive Library for Elixir",
+      paclage: package(),
+
+      # Docs
+      name: "ExNar",
+      docs: docs()
     ]
+
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
     [
       extra_applications: [:logger]
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+    ]
+  end
+
+  defp package do
+    [
+      maintainers: ["Austin Theriault"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => @source_url}
+    ]
+  end
+
+  defp docs do
+    [
+      source_url: @source_url
     ]
   end
 end
